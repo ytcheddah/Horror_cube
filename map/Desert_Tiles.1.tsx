374b5dce1fa82394832184478d58d854e4cf65b4
YTCheddah <peyton.grace2339@gmail.com>
2024-09-08 09:16:43 -0600

    added some new files

--- map/Desert_Tiles.1.tsx
+++ map/Desert_Tiles.1.tsx
@@ -1,44 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="Desert_Tiles.1" tilewidth="32" tileheight="32" tilecount="64" columns="8">
  <image source="../../Desktop/Desert_Tiles.1.png" width="256" height="256"/>
- <wangsets>
-  <wangset name="cliff" type="corner" tile="-1">
-   <wangcolor name="sand" color="#ff0000" tile="-1" probability="1"/>
-   <wangcolor name="cliff" color="#00ff00" tile="-1" probability="1"/>
-   <wangtile tileid="0" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="1" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="8" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="10" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="11" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="12" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="14" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="16" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="18" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="19" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="20" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="21" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="22" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="23" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="24" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="25" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="26" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="27" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="28" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="29" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="30" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="31" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="34" wangid="0,2,0,1,0,1,0,2"/>
-   <wangtile tileid="35" wangid="0,2,0,2,0,1,0,2"/>
-   <wangtile tileid="42" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="43" wangid="0,2,0,2,0,1,0,1"/>
-   <wangtile tileid="54" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="55" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="62" wangid="0,1,0,1,0,1,0,1"/>
-   <wangtile tileid="63" wangid="0,1,0,1,0,1,0,1"/>
-  </wangset>
- </wangsets>
 </tileset>
--- map_test.py
+++ map_test.py
@@ -5,6 +5,17 @@ from pytmx.util_pygame import load_pygame
 
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-tmx_data = load_pygame("map_test.tmx")
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+tmx_data = load_pygame("map/main_map.tmx")
 
+object_layer = tmx_data.get_layer_by_name('bushes')
+for obj in object_layer:
+    print(obj)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill('black')
+    pygame.display.update()
