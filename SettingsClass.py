1e5c326112827cb0e01341a1d00ce7827c00ec9e
YTCheddah <peyton.grace2339@gmail.com>
2024-11-02 10:33:26 -0600

    overflow sync

--- SettingsClass.py
+++ SettingsClass.py
@@ -1,8 +1,8 @@
 class Settings:
     """Class to manage variable user settings"""
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1280
+        self.screen_height = 720
         self.bg_color = (150, 35, 75)
 
         # Player settings 
